9402817833d14be155334901756331c3aa1a2dbc
JohnDoe <gamefake@live.nl>
2018-01-13 16:10:41 +0100

    Turned create connection into a promise because internal code is already a promise, and we need to wait for it before stopping the server

--- server/mongoDBConnector.js
+++ server/mongoDBConnector.js
@@ -2,18 +2,25 @@ const MongoClient = require('mongodb').MongoClient;
 const logging = require('../util/logging');
 const DB_NAME = "node_api";
 
-// Tries to create a connection with the local mongo DB server
-function createConnection() {
+/* Private Functions */
+// Tries to create a connection with the local MongoDB server
+function createMongoDBConnection(resolve, reject){
     MongoClient.connect('mongodb://127.0.0.1:27017/'+DB_NAME, (err, database) => {
         if(!err){
             logging.log("Successfully created connection to MongoDB!");
-            return true;
+            resolve(true);
         }else{
             logging.error("Error while creating connection to MongoDB!");
             logging.error(err);
-            return false;
+            resolve(false);
         }
     })
+}
+
+/* Public Functions */
+// Returns a promise
+function createConnection() {
+    return new Promise(createMongoDBConnection);
 }
 
 module.exports = {
